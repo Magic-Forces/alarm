2404ac5ce3759e34d960e1509a39fff69831ded5
Magic-Forces <root@foxlab.pl>
2025-06-27 13:58:00 +0200

    Refactor alarm state management and improve RTC health checks

--- src/main.cpp
+++ src/main.cpp
@@ -1,121 +1,114 @@
 #include <Arduino.h>
 #include <RtcDS1302.h>
 
-#define CH1 2 // RF receiver output (push button from remote)
+#define CH1 2
 // #define CH2 3
-#define PIR 4       // PIR sensor input (LOW when motion detected)
-#define REED 5      // Reed sensor input (LOW when door closed, HIGH when door opened)
-#define SIREN_PIN 6 // Alarm siren output pin (LOW = relay ON, HIGH = relay OFF)
+#define PIR 4
+#define REED 5
+#define SIREN 6
 // #define UPS 7
 // #define PUMP 8
-#define BUZZER 9 // Buzzer pin for state change notifications (LOW = relay ON, HIGH = relay OFF)
-#define CLK 10   // DS1302 Clock pin
-#define DAT 11   // DS1302 Data pin
-#define RST 12   // DS1302 Reset pin
-#define LED 13   // LED indicator (LOW = alarm on, HIGH = alarm off)
+#define BUZZER 9
+#define CLK 10
+#define DAT 11
+#define RST 12
+#define LED 13
 
-// System states
 enum AlarmState
 {
-    ALARM_OFF,      // System disarmed
-    ALARM_ARMED,    // System armed, waiting for trigger
-    ALARM_TRIGGERED // Alarm triggered, siren active
+    ALARM_OFF,
+    ALARM_ON,
+    ALARM_TRIGGERED
 };
 
-// Global variables
-AlarmState alarmState = ALARM_OFF;  // Current alarm state
-unsigned long lastButtonPress = 0;  // Timestamp of last remote button press
-unsigned long alarmTriggerTime = 0; // Timestamp when alarm was triggered
-unsigned long lastRtcCheck = 0;     // Timestamp of last auto-arm time check
-bool buttonPressed = false;         // Debounce flag for remote button
-bool autoArmedToday = false;        // Flag to ensure auto-arm happens only once per day
+AlarmState alarmState = ALARM_OFF;
+unsigned long lastButtonPress = 0;
+unsigned long alarmTriggerTime = 0;
+unsigned long lastRtcCheck = 0;
+unsigned long lastRtcHealthCheck = 0;
+bool buttonPressed = false;
+bool autoArmedToday = false;
+bool rtcReliable = true;
+bool rtcErrorPending = false;
 
-// Timing constants (milliseconds)
-const unsigned long DEBOUNCE_TIME = 100;         // Debounce time for RF receiver
-const unsigned long ALARM_DURATION = 180000;     // Siren duration:  3 minutes
-const unsigned long RTC_CHECK_INTERVAL = 600000; // Auto-arm check interval: 10 minutes
+const unsigned long DEBOUNCE_TIME = 100;
+const unsigned long ALARM_DURATION = 180000;
+const unsigned long RTC_CHECK_INTERVAL = 600000;
+const unsigned long RTC_HEALTH_CHECK_INTERVAL = 1800000;
 
-// RTC setup
-ThreeWire myWire(DAT, CLK, RST);  // ThreeWire interface for DS1302
-RtcDS1302<ThreeWire> Rtc(myWire); // RTC instance
+ThreeWire myWire(DAT, CLK, RST);
+RtcDS1302<ThreeWire> Rtc(myWire);
 
-// Function declarations
-void checkRemoteButton();                                             // Check if remote button was pressed
-void checkAutoArm();                                                  // Auto-arm logic after 22:00
-void armAlarm();                                                      // Manually arm the system
-void disarmAlarm();                                                   // Disarm the system
-void triggerAlarm();                                                  // Trigger the siren
-void beep(int times, int duration);                                   // Sound buzzer a number of times
-bool hasTimeElapsed(unsigned long startTime, unsigned long duration); // Timer helper
+void checkRemoteButton();
+void checkAutoArm();
+void armAlarm();
+void disarmAlarm();
+void triggerAlarm();
+void beep(int times, int duration);
+bool hasTimeElapsed(unsigned long startTime, unsigned long duration);
+bool checkRtcHealth();
+void tryFixRtc();
 
 void setup()
 {
-    // Configure input and output pins
     pinMode(CH1, INPUT_PULLUP);
     pinMode(PIR, INPUT_PULLUP);
     pinMode(REED, INPUT_PULLUP);
-    pinMode(SIREN_PIN, OUTPUT);
+    pinMode(SIREN, OUTPUT);
     pinMode(BUZZER, OUTPUT);
     pinMode(LED, OUTPUT);
 
-    // Initialize outputs to default OFF state (HIGH = relay OFF for LOW-active relays)
-    digitalWrite(SIREN_PIN, HIGH); // Relay OFF - siren disabled
-    digitalWrite(BUZZER, HIGH);    // Relay OFF - buzzer disabled
+    digitalWrite(SIREN, HIGH);
+    digitalWrite(BUZZER, HIGH);
 
-    // Initialize RTC module
     Rtc.Begin();
 
-    // --- RTC synchronization with compile time ---
     RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
     bool rtcError = false;
 
     if (!Rtc.IsDateTimeValid())
     {
         rtcError = true;
-        Rtc.SetDateTime(compiled); // Set to compile time if RTC invalid
+        Rtc.SetDateTime(compiled);
     }
     if (Rtc.GetIsWriteProtected())
     {
         rtcError = true;
-        Rtc.SetIsWriteProtected(false); // Allow writing time
+        Rtc.SetIsWriteProtected(false);
     }
     if (!Rtc.GetIsRunning())
     {
         rtcError = true;
-        Rtc.SetIsRunning(true); // Start RTC oscillator if stopped
+        Rtc.SetIsRunning(true);
     }
     RtcDateTime now = Rtc.GetDateTime();
     if (now < compiled)
     {
         rtcError = true;
-        Rtc.SetDateTime(compiled); // Correct RTC if it's behind compile time
+        Rtc.SetDateTime(compiled);
     }
 
-    // Signal RTC error if any occurred
+    rtcReliable = !rtcError;
+
     if (rtcError)
     {
-        beep(4, 250); // 4 medium beeps - RTC error
-        delay(500);
+        rtcErrorPending = true;
     }
-    // --- End RTC synchronization ---
 
-    // Immediately trigger alarm on startup
     triggerAlarm();
 }
 
 void loop()
 {
-    checkRemoteButton(); // Handle remote arming/disarming
-    checkAutoArm();      // Handle automatic arming at night
+    checkRemoteButton();
 
     switch (alarmState)
     {
     case ALARM_OFF:
-        // Do nothing when system is disarmed
+        checkAutoArm();
         break;
 
-    case ALARM_ARMED:
-        // If motion detected or door opened, trigger alarm
+    case ALARM_ON:
         if (digitalRead(PIR) == LOW || digitalRead(REED) == HIGH)
         {
             triggerAlarm();
@@ -123,19 +116,87 @@ void loop()
         break;
 
     case ALARM_TRIGGERED:
-        // After siren duration, stop siren and revert to ARMED
         if (hasTimeElapsed(alarmTriggerTime, ALARM_DURATION))
         {
-            digitalWrite(SIREN_PIN, HIGH); // Turn OFF siren relay
-            alarmState = ALARM_ARMED;
+            digitalWrite(SIREN, HIGH);
+            alarmState = ALARM_ON;
         }
         break;
     }
 }
 
+bool checkRtcHealth()
+{
+    if (!hasTimeElapsed(lastRtcHealthCheck, RTC_HEALTH_CHECK_INTERVAL))
+    {
+        return rtcReliable;
+    }
+    lastRtcHealthCheck = millis();
+
+    if (!Rtc.IsDateTimeValid())
+    {
+        return false;
+    }
+
+    if (!Rtc.GetIsRunning())
+    {
+        return false;
+    }
+
+    if (Rtc.GetIsWriteProtected())
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void tryFixRtc()
+{
+    bool fixed = false;
+
+    if (!Rtc.GetIsRunning())
+    {
+        Rtc.SetIsRunning(true);
+        fixed = true;
+    }
+
+    if (Rtc.GetIsWriteProtected())
+    {
+        Rtc.SetIsWriteProtected(false);
+        fixed = true;
+    }
+
+    if (fixed)
+    {
+        delay(100);
+        rtcReliable = checkRtcHealth();
+
+        if (rtcReliable)
+        {
+            rtcErrorPending = false;
+        }
+    }
+}
+
 void checkAutoArm()
 {
-    // Only check RTC periodically
+    if (!checkRtcHealth())
+    {
+        if (rtcReliable)
+        {
+            rtcReliable = false;
+            rtcErrorPending = true;
+            tryFixRtc();
+        }
+        return;
+    }
+
+    if (!rtcReliable)
+    {
+        return;
+    }
+
     if (!hasTimeElapsed(lastRtcCheck, RTC_CHECK_INTERVAL))
     {
         return;
@@ -144,18 +205,16 @@ void checkAutoArm()
 
     RtcDateTime now = Rtc.GetDateTime();
 
-    // Reset daily flag at midnight
     if (now.Hour() == 0 && autoArmedToday)
     {
         autoArmedToday = false;
     }
 
-    // Conditions: after 22:00, not yet auto-armed today, system OFF, door closed
-    if (now.Hour() >= 22 && !autoArmedToday && alarmState == ALARM_OFF && digitalRead(REED) == LOW)
+    if (now.Hour() >= 22 && !autoArmedToday && digitalRead(REED) == LOW)
     {
         beep(3, 150);
         delay(500);
-        alarmState = ALARM_ARMED;
+        alarmState = ALARM_ON;
         digitalWrite(LED, LOW);
         autoArmedToday = true;
     }
@@ -163,13 +222,11 @@ void checkAutoArm()
 
 void checkRemoteButton()
 {
-    // Debounce and detect falling edge on remote button
     if (digitalRead(CH1) == LOW && !buttonPressed && hasTimeElapsed(lastButtonPress, DEBOUNCE_TIME))
     {
         buttonPressed = true;
         lastButtonPress = millis();
 
-        // Toggle alarm state on button press
         if (alarmState == ALARM_OFF)
         {
             armAlarm();
@@ -181,60 +238,69 @@ void checkRemoteButton()
     }
     if (digitalRead(CH1) == HIGH)
     {
-        buttonPressed = false; // Reset debounce flag
+        buttonPressed = false;
     }
 }
 
 bool hasTimeElapsed(unsigned long startTime, unsigned long duration)
 {
-    return (millis() - startTime) >= duration; // Return true if specified duration passed
+    return (millis() - startTime) >= duration;
 }
 
 void armAlarm()
 {
-    // Prevent arming if door is open
     if (digitalRead(REED) == HIGH)
     {
-        beep(1, 1000);
+        beep(2, 1000);
         return;
     }
-    alarmState = ALARM_ARMED;
+    alarmState = ALARM_ON;
     digitalWrite(LED, LOW);
-    beep(1, 200);
+    beep(2, 200);
 }
 
 void triggerAlarm()
 {
     alarmState = ALARM_TRIGGERED;
     alarmTriggerTime = millis();
-    digitalWrite(SIREN_PIN, LOW); // Turn ON siren relay (LOW = active)
+    digitalWrite(SIREN, LOW);
 }
 
 void disarmAlarm()
 {
     alarmState = ALARM_OFF;
-    digitalWrite(SIREN_PIN, HIGH); // Turn OFF siren relay (HIGH = inactive)
+    digitalWrite(SIREN, HIGH);
     digitalWrite(LED, HIGH);
-    beep(2, 200);
-    RtcDateTime now = Rtc.GetDateTime();
-    // Reset daily auto-arm flag if after 22:00
-    if (now.Hour() >= 22)
+
+    beep(1, 200);
+
+    if (rtcErrorPending)
     {
-        autoArmedToday = false;
+        delay(300);
+        beep(1, 1000);
+        rtcErrorPending = false;
+    }
+
+    if (rtcReliable && checkRtcHealth())
+    {
+        RtcDateTime now = Rtc.GetDateTime();
+        if (now.Hour() >= 22)
+        {
+            autoArmedToday = false;
+        }
     }
 }
 
 void beep(int times, int duration)
 {
-    // Limit beep duration to maximum
     if (duration > 5000)
         duration = 5000;
     for (int i = 0; i < times; i++)
     {
-        digitalWrite(BUZZER, LOW); // Turn ON buzzer relay
+        digitalWrite(BUZZER, LOW);
         delay(duration);
-        digitalWrite(BUZZER, HIGH); // Turn OFF buzzer relay
-        if (i < times - 1)          // Don't delay after last beep
+        digitalWrite(BUZZER, HIGH);
+        if (i < times - 1)
         {
             delay(100);
         }
